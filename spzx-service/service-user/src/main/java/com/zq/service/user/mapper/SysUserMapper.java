b79319d9ac23d70892c91bd40cee7b49ba2c6dd7
zhangqiao <2996809820@qq.com>
2023-11-19 20:15:39 +0800

    Initial commit
    Signed-off-by: zhangqiao <2996809820@qq.com>

---
+++ spzx-service/service-user/src/main/java/com/zq/service/user/mapper/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.zq.service.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.spzx.model.entity.system.SysUser;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author 张乔
+ * @Date 2023/11/17 21:26
+ */
+@Mapper
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+
+
+}
